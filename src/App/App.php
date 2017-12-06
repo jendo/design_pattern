885b6446979dd2343bf2767dc663068c104d2e77
Michal Jenis <jenis.michal@gmail.com>
2017-12-06 14:27:44 +0100

    Basic skelet

---
+++ src/App/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MyApp\App;
+
+class App
+{
+    public function run()
+    {
+        var_dump('ok');
+    }
+}
--- www/index.php
+++ www/index.php
@@ -1,3 +1,9 @@
 <?php
+use Nette\DI\Container;
 
-phpinfo();
+/** @var Container $container */
+$container = require __DIR__ . '/../src/bootstrap.php';
+
+/** @var \MyApp\App\App $app */
+$app = $container->getService('application');
+$app->run();
